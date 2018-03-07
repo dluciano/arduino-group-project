af42687f23af25779f55b406fa92e1a4802bf574
Dawlin Peña Luciano <1720340@wlv.ac.uk>
2018-03-07 04:32:55 +0000

    Code refactoring and organized for extension

--- main/main.ino
+++ main/main.ino
@@ -4,6 +4,14 @@
 SoftwareSerial espSerial(3, 2);
 WiFiEspClient client;
 
+struct HttpResponse {
+  construct(String response){
+    int x = response.indexOf("\n\r") - 1;   
+    body = response.substring(x+4); //add 4 char, because from the last \n\r to the body there are 4 char (\n\r\n\r)
+  }
+  String body;
+};
+
 struct ConnMgr {
   void setup(){
     // Start esp wifi and its serial interface
@@ -49,32 +57,62 @@ struct ConnMgr {
     }
   }  
 
-  void loop(){
+  String loop(){
     // if there are incoming bytes available
     // from the server, read them and print them
-    while (requestDone && client.available()) {
-      char c = client.read();
-      Serial.write(c);
+    while (requestDone && client.available()) {      
+      msg.concat((char) client.read());
     } 
-    // if the server's disconnected, stop the client
+    // // if the server's disconnected, stop the client
     if (requestDone && !client.connected()) {
       Serial.println();
       Serial.println("Disconnecting from server...");
       client.stop();    
       Serial.println("Disconnected from server...");
       requestDone = false;
+      Serial.print("\nMessage:\n");
+      Serial.println(msg);
+      String aux = String(msg);
+      msg = "";
+      return aux;
     }
+    return "";
   }
 
   bool connected = false;
   bool requestDone = false;
+  String msg;
+};
+
+#define LED_PORT 13
+struct OperMgr{
+  void setup(){
+    pinMode(LED_PORT, HIGH);
+  }
+  void loop(HttpResponse message){
+    // Serial.print("\n\nHTTP RESPONSE BODY:<<<: ");
+    // Serial.print(message.body.c_str());    
+    // Serial.println(" :>>>");
+    if(message.body == "1"){
+      digitalWrite(LED_PORT, HIGH);
+    }else{
+      digitalWrite(LED_PORT, LOW);
+    }
+  }
 };
 
 ConnMgr conn;
+OperMgr op;
 
 void setup() { 
  Serial.begin(9600);
+ Serial.println("Configuring WIFI");
  conn.setup();
+ Serial.println("WIFI Configured");
+
+ Serial.println("Configuring Sensors and Components");
+ op.setup();
+ Serial.println("All Ready!");
 }
 
 #define  REFRESH_RATE 5000
@@ -87,5 +125,10 @@ void loop() {
     prev = last;
     conn.get("134.220.4.152", "/~1720203/test.php");  
   }
-  conn.loop();
+  String s = conn.loop();
+  if(s!= ""){
+    HttpResponse response;
+    response.construct(s);
+    op.loop(response);
+  }
 }
