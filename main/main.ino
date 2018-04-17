a258db9c1f5c5f4b761a636e3c282c63b432bfd8
Dawlin Peña Luciano <1720340@wlv.ac.uk>
2018-04-17 12:27:04 +0100

    HttpResponse class refactored

--- main/main.ino
+++ main/main.ino
@@ -3,55 +3,13 @@
 #include <SoftwareSerial.h>
 #include "LCDMgr.h"
 #include "Feedback.h"
+#include "HttpResponse.h"
 
 SoftwareSerial espSerial(3, 2);
 WiFiEspClient client;
-
 LCDMgr mLcd;
 
 
-struct HttpResponse {
-  construct(String response){
-    int x = response.indexOf("\n\r") - 1;   
-    body = response.substring(x+4); //add 4 char, because from the last \n\r to the body there are 4 char (\n\r\n\r)
-  }
-  String body;
-
-  Feedback** asFeedback(){
-    if(body == "")
-      return NULL;
-    
-    int feedbacksCount = 1;    
-    for(int i = 0; i < body.length(); i++){      
-      if(body.charAt(i) == '|'){
-        feedbacksCount++;       
-      }
-    }    
-
-    char* feedbacksStr = strtok(body.c_str(), "|");    
-    char** fbs = new char*[feedbacksCount];
-    int i = 0;
-    while(feedbacksStr != NULL){
-      fbs[i] = feedbacksStr;
-      feedbacksStr = strtok(NULL, "|");
-      i++;
-    }
-    
-    Feedback** feedbacks = new Feedback*[feedbacksCount + 1];    
-    char *f = NULL;
-    for(int x = 0; x < i; x++){      
-      f = strtok(fbs[x], ":");
-      int userId = atoi(f);
-      f = strtok(NULL, ":");
-      int oper = atoi(f);
-      feedbacks[x] = new Feedback(userId, oper);      
-    }
-    //TODO: Delete the fbs variable    
-    feedbacks[feedbacksCount]  = NULL;
-
-    return feedbacks;
-  }
-};
 
 class ConnMgr {  
   public:
@@ -181,8 +139,7 @@ void loop() {
   }
   String s = conn.loop();
   if(s != ""){
-    HttpResponse response;
-    response.construct(s);    
+    HttpResponse response(s);      
     Feedback** fbs = response.asFeedback();
     op.loop(fbs);
 
